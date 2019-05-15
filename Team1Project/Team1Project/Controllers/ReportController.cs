821b178a3629cce6bf511de5f317335a967bb4fa
Ho Wing Wong <wongh5@seattleu.edu>
2019-05-15 13:57:06 -0700

    added contact page

---
+++ Team1Project/Team1Project/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team1Project.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
